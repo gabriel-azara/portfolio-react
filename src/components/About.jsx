0f331e390f661213ee88a646c61fbc7faf503285
Gabriel Azara <gabriel1190@gmail.com>
2023-09-08 09:46:16 -0300

    images added and improvements

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import LanguageSelector from './LanguageSelector';
 import { useTranslation } from 'react-i18next';
 import Timeline from './Timeline';
-import "./About.css"
+import './About.css';
 const About = () => {
   const { t } = useTranslation();
 
@@ -16,32 +16,31 @@ const About = () => {
         {/* <p id="bio">{t('about.bio')}</p> */}
         <div className="sections">
           <h3>{t('about.education')}</h3>
-        <ul>
-          <li>{t('about.educationItem1')}</li>
-          <li>{t('about.educationItem2')}</li>
-        </ul>
+          <ul>
+            <li>{t('about.educationItem1')}</li>
+            <li>{t('about.educationItem2')}</li>
+            <li>{t('about.educationItem3')}</li>
+          </ul>
         </div>
         <div className="sections">
-
-        <h3>Hard Skills</h3>
-        <ul>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
-          <li>React.js</li>
-          <li>WordPress</li>
-          <li>Bootstrap</li>
-        </ul>
+          <h3>Hard Skills</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>React.js</li>
+            <li>WordPress</li>
+            <li>Bootstrap</li>
+          </ul>
         </div>
         <div className="sections">
-
-        <h3>Soft Skills</h3>
-        <ul>
-          <li>{t('about.softSkill1')}</li>
-          <li>{t('about.softSkill2')}</li>
-          <li>{t('about.softSkill3')}</li>
-        </ul>
+          <h3>Soft Skills</h3>
+          <ul>
+            <li>{t('about.softSkill1')}</li>
+            <li>{t('about.softSkill2')}</li>
+            <li>{t('about.softSkill3')}</li>
+          </ul>
         </div>
       </section>
     </div>
--- src/components/AboutThis.jsx
+++ src/components/AboutThis.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './AboutThis.css';
 import img1 from '../assets/images/components.png';
+import img2 from '../assets/images/themecontext.png';
+import img3 from '../assets/images/themetoggle.png';
 import { useTranslation } from 'react-i18next';
 
 const AboutThis = () => {
@@ -8,14 +10,10 @@ const AboutThis = () => {
   return (
     <section id="documentation">
       <h2>{t('portfolio.title')}</h2>
-      <p className="documentationIntro">
-      {t('portfolio.titleText')}
-      </p>
+      <p className="documentationIntro">{t('portfolio.titleText')}</p>
       <div className="sections">
         <h3>{t('portfolio.about')}</h3>
-        <p>
-        {t('portfolio.aboutText')}
-        </p>
+        <p>{t('portfolio.aboutText')}</p>
       </div>
       <div className="sections">
         <h3>{t('portfolio.wasUsed')}</h3>
@@ -23,11 +21,16 @@ const AboutThis = () => {
           <li>
             <h3>{t('portfolio.components')}</h3>
             <p>{t('portfolio.componentsText')}</p>
+            <h5>App.js</h5>
             <img className="imgPortfolio" src={img1} alt="" />
           </li>
           <li>
-            <h3>{t('portfolio.themeChange')}</h3>
-            <p>{t('portfolio.themeChangeText')}</p>
+            <h3>{t('portfolio.themeContext')}</h3>
+            <p>{t('portfolio.themeContextText')}</p>
+            <h5>ThemeContext.jsx</h5>
+            <img className="imgPortfolio" src={img2} alt="" />
+            <h5>ThemeToggle.jsx</h5>
+            <img className="imgPortfolio" src={img3} alt="" />
           </li>
           <li>
             <h3>{t('portfolio.languageToggle')}</h3>
